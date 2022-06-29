a58639b597c7d53e0e67ffcf73452850fc545acd
Surbhi-SD <99402618+Surbhi-SD@users.noreply.github.com>
2022-06-29 15:28:04 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,12 @@
+var first = prompt("enter first number");
+var second = prompt("enter second number");
+
+var sum = Number(first) + Number(second);
+var sub = Number(first) - Number(second);
+var mul = Number(first) * Number(second);
+var div = Number(first) / Number(second);
+
+alert("The sum is:" + sum);
+alert("The difference is:" + sub);
+alert("The product is:" + mul);
+alert("The division is:" + div);
